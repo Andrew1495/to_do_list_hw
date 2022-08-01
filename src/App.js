f28a7f9f37f1d74c535833deba2a0d3b4c0e306f
Andrew <andrewfairley95@gmail.com>
2022-08-01 14:58:54 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [tasks, setTasks] = useState([]);
+
+  const [newTask, setNewTask] = useState("")
+  
+  const [newPrio, setPrio] = useState("")
+
+
+  
+
+  const taskNode = tasks.map((task, index) => { 
+    return(
+      <li key={index} className={task.priority}>
+        <span>
+            {task.name}
+        </span>
+        <span>
+            {task.priority}
+        </span>
+      </li>
+    )
+  });
+
+  const handlePrioInput = (event) =>{
+    setPrio(event.target.value)
+  }
+
+
+  const handleTaskInput = (event) =>{
+    setNewTask(event.target.value)
+  };
+
+
+const saveTask = (event) => {
+  event.preventDefault()
+  const copyTask = [...tasks];
+  copyTask.push({name: newTask, priority: newPrio });
+  setTasks((copyTask))
+  setNewTask("");
 }
+
+
+  return(
+    <div className="app">
+      <h1>To do list</h1>
+
+
+
+    <form onSubmit={saveTask}>
+
+
+      <label htmlFor='new-task'>Add to list</label>
+
+      <input type="text"
+      id='new-task'
+      value={newTask}
+      onChange= {handleTaskInput}
+      placeholder="Add to list" 
+      required/>
+
+      <label className='option 1'>
+            <input
+              type="radio"
+              name="prio"
+              value="High"
+              className="form-check-input"
+              onChange={handlePrioInput}
+              required
+            />
+            High
+        </label>
+      <label className='option 2'>
+            <input
+              type="radio"
+              name="prio"
+              value="Low"
+              className="form-check-input"
+              onChange={handlePrioInput}
+              required
+            />
+            Low
+        </label>
+
+
+  
+      <input type="submit" value="Add"/>
+    </form>
+
+
+    <hr/>
+    <ul>
+      {taskNode}
+    </ul>
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+
+
+
+};
 
 export default App;
